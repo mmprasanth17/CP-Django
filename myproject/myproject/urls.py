75a5d0ab09827bbb70e5b8376faee1408ee31235
mmprasanth17 <prasanthmani1711@gmail.com>
2024-08-12 11:16:31 +0530

    change

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myproject\Task\weekweek/', include("week.urls")),
+    path('admin/', admin.site.urls),
+    path('month/', include('month.urls')),
+    path('week/', include('week.urls')),
 ]
